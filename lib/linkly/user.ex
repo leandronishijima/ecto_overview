7bcaee944b82b8940971cdba4725b54a86348d7b
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-08-01 21:17:06 -0300

    +schemas associations (belongs_to and has_many)

--- lib/linkly/user.ex
+++ lib/linkly/user.ex
@@ -1,10 +1,13 @@
 defmodule Linkly.User do
   use Ecto.Schema
 
+  alias Linkly.Bookmark
+
   schema "users" do
     field(:about)
     field(:email)
     field(:username)
+    has_many(:bookmarks, Bookmark)
 
     timestamps()
   end
